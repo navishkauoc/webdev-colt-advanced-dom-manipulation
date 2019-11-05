1f87f41883406f76655c15a8b0b99f25db467703
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-05 13:08:19 +0530

    Working only on one way

--- Color Changer/changer.js
+++ Color Changer/changer.js
@@ -1,1 +1,9 @@
-alert("Hello!");
+var butt = document.querySelector("body");
+
+butt.addEventListener("click",changeBgroundClr);
+
+function changeBgroundClr(){
+    //var isWhite = true;
+    butt.style.background = "blue"; //isWhite ? blue : white;
+    //isWhite = false;
+}
