4008feaf40479ea7684f88a06d1553783ecae358
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-05 11:42:40 +0530

    Update dom01.js

--- dom01.js
+++ dom01.js
@@ -2,4 +2,5 @@ var e01 = document.querySelector("h1");
 console.log(e01);
 e01.addEventListener("click",function(){
     console.log("SOMEONE CLICKED THE H1");
+    alert("You clicked the H1");
 });
