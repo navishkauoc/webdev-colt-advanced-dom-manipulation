40d9998979a6001d3099258e921bf703f7c4bd4b
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-06 15:05:40 +0530

    Reset  button

--- Score Keeper/scoreKeeper.js
+++ Score Keeper/scoreKeeper.js
@@ -1,5 +1,6 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Disp = document.getElementById("p1Display");
 var p2Disp = document.getElementById("p2Display");
 var p1Score = 0;
@@ -29,3 +30,12 @@ p2Button.addEventListener("click", function(){
         p2Disp.textContent = p2Score;
     }
 });
+
+resetButton.addEventListener("click",function(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Disp.textContent = 0;
+    p2Disp.textContent = 0;
+    p1Disp.classList.remove("winner");
+    p2Disp.classList.remove("winner");
+})
