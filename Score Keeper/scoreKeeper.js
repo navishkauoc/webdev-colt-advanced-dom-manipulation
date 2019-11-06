85524704550f75589edfeecbf1114a382d9650ae
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-06 14:10:41 +0530

    Player 1 working

--- Score Keeper/scoreKeeper.js
+++ Score Keeper/scoreKeeper.js
@@ -1,8 +1,11 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var p1Disp = document.getElementById("p1Display");
+var p2Disp = document.getElementById("p2Display");
 var p1Score = 0;
 
 p1Button.addEventListener("click", function(){
     p1Score++;
     console.log(p1Score);
-})
+    p1Disp.textContent = p1Score;
+});
