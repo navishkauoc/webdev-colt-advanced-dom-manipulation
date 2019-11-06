70fa219ebc8d40ae18967696c0ceae05230184c0
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-06 14:29:22 +0530

    Winnig conditions added

--- Score Keeper/scoreKeeper.js
+++ Score Keeper/scoreKeeper.js
@@ -4,14 +4,26 @@ var p1Disp = document.getElementById("p1Display");
 var p2Disp = document.getElementById("p2Display");
 var p1Score = 0;
 var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
 
 p1Button.addEventListener("click", function(){
-    p1Score++;
-    console.log(p1Score);
-    p1Disp.textContent = p1Score;
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+            gameOver = true;
+        }
+        p1Disp.textContent = p1Score;
+    }
+   
 });
 
 p2Button.addEventListener("click", function(){
-    p2Score++;
-    p2Disp.textContent = p2Score;
+    if(!gameOver){
+        p2Score++;
+        if(p2Score === winningScore){
+            gameOver = true;
+        }
+        p2Disp.textContent = p2Score;
+    }
 });
