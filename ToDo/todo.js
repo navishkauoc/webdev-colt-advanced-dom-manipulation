a21346b105de62141b65e607799a9776a7f5961c
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-06 17:57:29 +0530

    Final

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -1,9 +1,13 @@
-var firstLi = document.querySelector("li");
+var lis = document.querySelectorAll("li");
 
-firstLi.addEventListener("mouseover",function(){
-    firstLi.style.color = "green";
-});
-
-firstLi.addEventListener("mouseout", function(){
-    firstLi.style.color = "black";
-})
+for(var i=0; i<lis.length; i++){
+    lis[i].addEventListener("mouseover",function(){
+        this.style.color = "green";
+    });
+    lis[i].addEventListener("mouseout",function(){
+        this.style.color = "black";
+    });
+    lis[i].addEventListener("click",function(){
+        this.classList.toggle("done");
+    });
+}
